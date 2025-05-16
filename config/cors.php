543fecd971634634280270d95af9b47f4aebccdd
An Dev <annh@qasoftsolution.com>
2025-05-16 17:24:03 +0700

    env

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    // 'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://spending-fe-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
